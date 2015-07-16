362b567a3ef4892bf27d0d03b3c5699236a87b2d
subhak186 <subhak@juniper.net>
2015-07-16 18:38:56 +0530

    Update slick.js

--- slick/slick.js
+++ slick/slick.js
@@ -1668,6 +1668,10 @@
 
         _.$slidesCache = _.$slides;
 
+        _.$slides.each(function(index, element) {
+	        	$(element).attr('data-slick-index', index);
+	        });
+	        
         _.reinit();
 
     };
